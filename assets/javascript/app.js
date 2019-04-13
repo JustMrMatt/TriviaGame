b13a5d20b0191b238b23f8af25c0b7f57508bacb
Matthew Allen <matthewallenunc@gmail.com>
2019-04-13 01:12:07 -0400

    Questions written out

---
+++ assets/javascript/app.js
@@ -0,0 +1,47 @@
+$(document).ready(function () {
+    var questions = [
+        {
+            question: "What is Darth Vader's true name?", 
+            options: ["Darth Vader", "Jim Stargazer", "Anakin Skywalker", "Harry Swordswinger" ],
+            answer: "Anakin Skywalker",
+        },
+        {
+            question: "About how old is Jedi Master Yoda?", 
+            options: ["700 years", "800 years", "900 years", "1000 years" ],
+            answer: "900 years",
+        },
+        {
+            question: "What color is Yoda's Lightsaber?", 
+            options: ["Red", "Green", "Purple", "Blue" ],
+            answer: "Green",
+        },
+        {
+            question: "Who is Luke Skywalker's father?", 
+            options: ["Darth Vader", "Darth Sidious", "Darth Maul", "Qui-Gon Jin" ],
+            answer: "Darth Vader",
+        },
+        {
+            question: "What planet is Rey from?", 
+            options: ["Tatooine", "Hoth", "Naboo", "Jakku" ],
+            answer: "Jakku",
+        },
+        {
+            question: "What color lightsaber does Mace Windu use?", 
+            options: ["Red", "Green", "Purple", "Blue" ],
+            answer: "Purple",
+        },
+        {
+            question: "Who is Kylo Ren's father?", 
+            options: ["Darth Vader", "Luke Skywalker", "JarJar Binks", "Han Solo" ],
+            answer: "Han Solo",
+        },
+        {
+            question: "What bounty hunter caught Han Solo?", 
+            options: ["Jango Fett", "Boba Fett", "Lando Calrissian", "Darth Maul" ],
+            answer: "Bob Fett",
+        },
+        {
+            question: "JarJar Binks' true identity is?", 
+            options: ["Jedi Master", "Bounty Hunter", "Rebel Spy", "Sith Lord" ],
+            answer: "Sith Lord",
+        }];
